d1081739c4bd2d4e3c4d4d75a005024f3fa70a42
Dmitry <grayeyed16@gmail.com>
2019-07-29 15:35:04 +0300

    Add open conversation link to appointment list (#4)

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,6 +5,9 @@ class Appointment < ApplicationRecord
   belongs_to :physician
 
   validates :attendance_date, presence: true
-
   enum status: %i[requested canceled rejected accepted attended]
+
+  def conversation
+    Conversation.first_or_create(patient: patient, physician: physician)
+  end
 end
