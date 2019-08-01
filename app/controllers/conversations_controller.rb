b4a92a5f38efd73b75b283c5f703d7bc609544a0
Dmitry Syvoglaz <grayeyed16@gmail.com>
2019-08-01 10:41:32 +0300

    Update bugno to 0.1.3

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -4,6 +4,7 @@ class ConversationsController < ApplicationController
   expose :conversation
   expose :conversations, -> { current_user.conversations }
   expose :message, parent: :conversation, id: :message_id
+  expose :last_message, -> { conversation.messages.last }
   expose :messages, -> { conversation.messages }
 
   def index; end
