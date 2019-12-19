4010ebb2e719e83d8c53a75f0fab8e12b8035455
Dmitry Sivoglaz <grayeyed16@gmail.com>
2019-12-19 18:35:51 +0200

    Move bugno api key to env in initializer

--- config/initializers/bugno.rb
+++ config/initializers/bugno.rb
@@ -1,23 +1,6 @@
 Bugno.configure do |config|
-  # Specify api key to send exception to Bugno
-  config.api_key = 'JRiZ3t2w3dRWTAmF1UFPxnL5'
-
-  # Send in background with threading:
+  config.api_key = ENV['BUGNO_API_KEY']
   config.send_in_background = true
-
-  # Skip rails related exceptions:
-  config.exclude_rails_exceptions = true
-
-  # Specify which rails exception to skip:
-  config.excluded_exceptions = []
-
-  # Specify or add usage environments:
-  config.usage_environments = %w[production development]
-
-  # Specify current user method:
+  config.usage_environments = %w[production]
   config.current_user_method = 'current_user'
-
-  # Add scrub fields and headers:
-  # config.scrub_fields << 'password'
-  # config.scrub_headers << 'access_token'
 end
