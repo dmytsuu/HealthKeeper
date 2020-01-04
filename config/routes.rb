b421123dc238a40f78c1d6a2e98800682a52b3b6
Dmitry Sivoglaz <grayeyed16@gmail.com>
2020-01-04 20:48:29 +0200

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,13 @@
 
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
-
   mount ActionCable.server, at: '/cable'
 
   root 'pages#home'
   get 'pages/home'
   get 'dashboard', to: 'dashboard#index'
 
-  devise_for :physicians, controllers: {
-    sessions: 'physicians/sessions'
-  }
-
+  devise_for :physicians, controllers: { sessions: 'physicians/sessions' }
   devise_for :patients, controllers: {
     sessions: 'patients/sessions',
     registrations: 'patients/registrations'
