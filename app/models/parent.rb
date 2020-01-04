29b5d4aeaf86ce74bc71f630b893cd7d1c4277c7
Dmitry Sivoglaz <grayeyed16@gmail.com>
2020-01-04 20:48:52 +0200

    Refacotor parents factory and parent model with rubocop

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Parent < ApplicationRecord
   belongs_to :patient
 end
--- spec/factories/parents.rb
+++ spec/factories/parents.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :parent do
-    name { "MyString" }
-    surname { "MyString" }
+    name { 'MyString' }
+    surname { 'MyString' }
     patient { nil }
   end
 end
